f9ccd10c503287f153903e81965f368c0ff04a66
laipuran <55612059+laipuran@users.noreply.github.com>
2023-04-29 16:49:18 +0800

    添加项目文件。

---
+++ WebApi/Controllers/DeviceStatusController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeviceStatusController : ControllerBase
+    {
+        private readonly ILogger<DeviceStatusController> _logger;
+
+        public DeviceStatusController(ILogger<DeviceStatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetDeviceStatus")]
+        public DeviceStatus Get()
+        {
+            string ipaddress = "";
+            string name = Dns.GetHostName();
+            IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
+            foreach (IPAddress ipa in ipadrlist)
+            {
+                if (ipa.AddressFamily == AddressFamily.InterNetworkV6)
+                    ipaddress = ipa.ToString();
+            }
+            return new DeviceStatus() {
+                Ip = ipaddress,
+                Time = TimeOnly.FromDateTime(DateTime.Now)
+            };
+        }
+    }
+}
---
+++ WebApi/DeviceStatus.cs
@@ -0,0 +1,9 @@
+namespace WebApi
+{
+    public class DeviceStatus
+    {
+        public TimeOnly Time { get; set; }
+
+        public string? Ip { get; set; }
+    }
+}
